bbbc0aee5efd031f45dfced1f741ee02c0f2305c
Virennaruka <113277515+Virennaruka@users.noreply.github.com>
2023-01-22 15:49:22 +0530

    Create thumbfuc.py

---
+++ Plugins/thumbfuc.py
@@ -0,0 +1,24 @@
+from pyrogram import Client, filters
+from helper.database import db
+
+@Client.on_message(filters.private & filters.command(['viewthumb']))
+async def viewthumb(client, message):    
+    thumb = await db.get_thumbnail(message.from_user.id)
+    if thumb:
+       await client.send_photo(
+	   chat_id=message.chat.id, 
+	   photo=thumb)
+    else:
+        await message.reply_text("😔 __**YOU DONT HAVE ANY THUMBNAIL**__") 
+		
+@Client.on_message(filters.private & filters.command(['delthumb']))
+async def removethumb(client, message):
+    await db.set_thumbnail(message.from_user.id, file_id=None)
+    await message.reply_text("❌️ __**SUCCESSFULLY DELETED**__")
+	
+@Client.on_message(filters.private & filters.photo)
+async def addthumbs(client, message):
+    mkn = await message.reply_text("Please Wait ...")
+    await db.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
+    await mkn.edit("✅️ __**SUCCESSFULLY SAVED**__")
+	
