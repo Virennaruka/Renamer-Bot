ddb93d21e4d2ff3b5e33ae00668a0d48b0e758c9
Virennaruka <113277515+Virennaruka@users.noreply.github.com>
2023-01-22 15:06:10 +0530

    Create txt.py

---
+++ helper/txt.py
@@ -0,0 +1,38 @@
+class mr(object):
+    PROGRESS_BAR = """\n
+┏━━━━━━━━━━━━━━━━━━━━━
+┣ ⏳️ PERCENTAGE: {0}%
+┣ 🗂️ PROGRESS: {1} | {2}
+┣ 🚀 SPEED: {3}/s
+┣ ⏱️ ESTIMATED TIME: {4}
+┗━━━━━━━━━━━━━━━━━━━━━ """
+
+    ABOUT_TXT = """
+┏━━━━━━━━━━━━━━━━━━━━━
+‣🔸 MY NAME : {}
+‣🔸 DEVELOPERS : <a href=https://t.me/The_Maniacs>The Maniacs</a> 
+‣🔸 LIBRARY : <a href=https://github.com/pyrogram>𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼</a>
+‣🔸 LANGUAGE : <a href=https://www.python.org>𝙿𝚈𝚃𝙷𝙾𝙽 3</a>
+‣🔸 DATA BASE : <a href=https://cloud.mongodb.com>𝙼𝙾𝙽𝙶𝙾𝙳𝙱</a>
+‣🔸 BUILD STATUS : v2.1.90 [ ꜱɪɢᴍᴀ 🗿 ]              
+┗━━━━━━━━━━━━━━━━━━━━━
+                                """
+    HELP_TXT = """
+🌌 <b><u>HOW TO SET THUMBNAIL</u></b>
+  
+•> /start a bot and send any picture to automatically set thumbnail.
+•> /delthumb use this command and delete your old thumbnail.
+•> /viewthumb use this command view your current thumbnail.
+
+📑 <b><u>HOW TO SET CUSTOM CAPTION</u></b>
+•> /set_caption - set a custom caption
+•> /see_caption - see your custom caption
+•> /del_caption - delete custom caption
+
+Example:- /set_caption 📕 File Name: {filename}
+💾 Size: {filesize}
+⏰ Duration: {duration}
+
+✏️ <b><u>HOW TO RENAME A FILE</u></b>
+•> send any file and click rename option and type new file name and \n send select [ document, video, audio ]👈 choice this.
+"""
