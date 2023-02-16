436c8554c39be87e84e7b097f1a02d4dc4af9548
Virennaruka <113277515+Virennaruka@users.noreply.github.com>
2023-02-16 13:35:52 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -3,7 +3,7 @@ from config import DB_URL, DB_NAME
 
 class Database:
 
-    def __init__(self, uri, database_name):
+    def __init__(self, url, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
         self.col = self.db.user
